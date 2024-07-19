b4364fb53ad6d58a1a9cb1da4ee5238f11739c6a
Edil Medeiros <jose.edil@gmail.com>
2024-07-18 21:18:43 -0300

    Make format_uptime function a local function

--- src/commands.rs
+++ src/commands.rs
@@ -7,17 +7,19 @@ pub fn get_best_block_hash(rpc: &Client) -> Result<BlockHash, Error> {
 }
 
 pub fn get_uptime(rpc: &Client) -> Result<String, Error> {
+    fn format_uptime(uptime_seconds: u64) -> String {
+        let days = uptime_seconds / 86_400;
+        let hours = (uptime_seconds % 86_400) / 3600;
+        let minutes = (uptime_seconds % 3600) / 60;
+        let seconds = uptime_seconds % 60;
+
+        format!("{days} days, {hours} hours, {minutes} minutes, {seconds} seconds")
+    }
+
     let uptime_seconds = rpc.uptime()?;
     let formatted_uptime = format_uptime(uptime_seconds);
     println!("uptime: {}", &formatted_uptime);
     Ok(formatted_uptime)
 }
 
-fn format_uptime(uptime_seconds: u64) -> String {
-    let days = uptime_seconds / 86_400;
-    let hours = (uptime_seconds % 86_400) / 3600;
-    let minutes = (uptime_seconds % 3600) / 60;
-    let seconds = uptime_seconds % 60;
-
-    format!("{days} days, {hours} hours, {minutes} minutes, {seconds} seconds")
 }
